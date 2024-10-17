e1daa27758020ddb7c512c9885a5cc4603fb3f55
nishantssoni <iamnishantsoni4@gmail.com>
2024-10-17 14:08:58 +0530

    added cuda

--- minGPT/Bigram.py
+++ minGPT/Bigram.py
@@ -9,6 +9,7 @@ max_iters = 3000
 eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("current device running on :: ", device )
 eval_iters = 200
 
 # -------------------------------
@@ -42,6 +43,7 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
     return x, y
 
 
