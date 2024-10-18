49bbdac94959880fa1bd0fd39631199461e5cd26
nishantssoni <iamnishantsoni4@gmail.com>
2024-10-18 10:23:04 +0530

    added multihead attention with feed forward layer

--- minGPT/V2.py
+++ minGPT/V2.py
@@ -88,13 +88,40 @@ class Head(nn.Module):
         return out
 
  
+class MultipleHeadAttention(nn.Module):
+    """Multiple heads of self attention"""
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+
+
 # simple bigram language model loading
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_head = MultipleHeadAttention(4, n_embd//4) #4 heads of 8 dim attention
+        self.ffwd = FeedFoward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
@@ -103,6 +130,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T, C)
         x = token_emb + pos_emb #(B, T, C)
         x = self.sa_head(x)
+        x = self.ffwd(x)
         logits = self.lm_head(x) #(B, T, vocab_size)
         
         if targets is None:
