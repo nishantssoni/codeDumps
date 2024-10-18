f067fde94cc5e9c5039c3dc40d37327df53e9369
nishantssoni <iamnishantsoni4@gmail.com>
2024-10-18 10:58:10 +0530

    added bloack layer

--- minGPT/V2.py
+++ minGPT/V2.py
@@ -111,7 +111,20 @@ class FeedFoward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
 
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultipleHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+
+    def forward(self, x):
+        x =self.sa(x)
+        x =self.ffwd(x)
+        return x
 
 
 # simple bigram language model loading
@@ -120,8 +133,11 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = MultipleHeadAttention(4, n_embd//4) #4 heads of 8 dim attention
-        self.ffwd = FeedFoward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
@@ -129,8 +145,7 @@ class BigramLanguageModel(nn.Module):
         token_emb = self.token_embedding_table(idx)  #(B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T, C)
         x = token_emb + pos_emb #(B, T, C)
-        x = self.sa_head(x)
-        x = self.ffwd(x)
+        x = self.blocks(x)
         logits = self.lm_head(x) #(B, T, vocab_size)
         
         if targets is None:
