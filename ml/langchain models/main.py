736c602c076f9411b62b8eef6c67dc2bd9290384
nishantssoni <iamnishantsoni4@gmail.com>
2025-06-07 13:03:57 +0530

    added gemeni code

--- ml/langchain models/main.py
+++ ml/langchain models/main.py
@@ -1,1 +1,10 @@
 
+from langchain_core.messages import HumanMessage
+from langchain_community.chat_models import ChatOllama
+
+chat = ChatOllama(model="deepseek-r1:1.5b")
+
+response = chat.invoke([
+    HumanMessage(content="What's the capital of France?")
+])
+print(response)
