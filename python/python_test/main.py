14a29a3f7788e7ba76827298b498142d95b7896c
nishantssoni <iamnishantsoni4@gmail.com>
2026-06-02 12:18:35 +0530

    added test code for value error testion

--- python/python_test/main.py
+++ python/python_test/main.py
@@ -1,4 +1,7 @@
-def get_weather(temp):
-    if temp > 20:
-        return "hot"
-    return "cold"
+def add(a, b):
+    return a+b
+
+def divide(a, b):
+    if b==0:
+        raise ValueError("cannot divide by zero")
+    return a/b
--- python/python_test/test_main.py
+++ python/python_test/test_main.py
@@ -1,4 +1,12 @@
-from main import get_weather
+from main import add, divide
+import pytest
 
-def test_get_weather():
-    assert get_weather(15) == "hot"
+
+def test_add():
+    assert add(2,3) == 5, "2 + 3 shoud be 5"
+    assert add(-1,1) == 0, "-1 + -1 should be 0"
+    assert add(0,0) == 0, "0 + 0 should be 0"
+
+def test_divide():
+    with pytest.raises(ValueError, match="cannot divide by zero"):
+        divide(10,0)
